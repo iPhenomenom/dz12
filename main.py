5a3e5b30bb261825dd730091a2e610cb42d1100f
Dmiriy <mytoreh@gmail.com>
2023-06-22 14:58:16 +0300

    +

--- main.py
+++ main.py
@@ -8,13 +8,51 @@ from pydantic import BaseModel, EmailStr, Field
 from datetime import date, timedelta
 import uvicorn
 import os
+from datetime import datetime, timedelta
+import jwt
+from jwt import PyJWTError
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.testing import db
+from starlette import status
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
-SQLALCHEMY_DATABASE_URL = "postgresql://User:password@localhost:5432/newDB"
+Base = declarative_base()
+
+# SQLAlchemy model
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+# Pydantic models
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://User:Password@localhost:5432/newDB"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
 
 class Contact(Base):
     __tablename__ = "contacts"
@@ -26,7 +64,6 @@ class Contact(Base):
     phone = Column(String)
     birthday = Column(Date)
     additional_info = Column(String)
-
 
 Base.metadata.create_all(bind=engine)
 
@@ -60,6 +97,13 @@ class ContactInDB(BaseModel):
 
     class Config:
         orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
 
 def get_db():
     db = SessionLocal()
@@ -127,10 +171,92 @@ def get_upcoming_birthdays(db: Session = Depends(get_db)):
     contacts = db.query(Contact).filter((Contact.birthday >= today) & (Contact.birthday <= next_week)).all()
     return contacts
 
-#@app.get("/")
-#def read_root():
-#    return {"Hello": "World"}
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def create_user(db: Session, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(*, data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except PyJWTError:
+        raise credentials_exception
+    user = get_user_by_email(db, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
+
+@app.post("/users/", response_model=UserInDB, status_code=201)
+def create_user_endpoint(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=409, detail="Email already registered")
+    return create_user(db=db, user=user)
+
+@app.post("/token", response_model=Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, email=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
-
